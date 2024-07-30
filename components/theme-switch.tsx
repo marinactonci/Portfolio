786f8797f6b0309195c1f9c95f466c99d25f22e0
Tonči Marinac <toncimarinac@gmail.com>
2024-07-30 18:01:19 +0200

    fix for experience link in nav not being active on mobile when in view, theme switch now hidden when scrolled to the bottom of the page on mobile

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,15 +1,29 @@
 "use client";
 
 import { useTheme } from "@/context/theme-context";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
+  const [isBottom, setIsBottom] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.innerHeight + window.scrollY;
+      const threshold = document.body.offsetHeight - 140;
+      setIsBottom(scrollPosition >= threshold);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <button
-      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      className={`fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 ${
+        isBottom && "hidden sm:block"
+      }`}
       onClick={toggleTheme}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
